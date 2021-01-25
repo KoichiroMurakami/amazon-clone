4627a2b227580d776fd2aee9f9208c4e17be5d70
Rick516 <k.murakami516@gmail.com>
2021-01-25 18:53:27 +0900

    css config modified

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -1,58 +1,56 @@
-/* eslint-disable eol-last */
-/* eslint-disable indent */
 export default {
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'admin',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            {
-                rel: 'stylesheet',
-                href: '/css/font-awesome/all.css'
-            },
-            {
-                rel: 'stylesheet',
-                href: '/css/font-awesome/default.css'
-            }
-        ]
-    },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [],
-
-    srcDir: 'src',
-
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
-
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
-
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        // https://go.nuxtjs.dev/bootstrap
-        'bootstrap-vue/nuxt',
-        // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios',
-        // https://go.nuxtjs.dev/pwa
-        '@nuxtjs/pwa'
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'admin',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: '/css/font-awesome/css/all.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: '/css/default.css'
+      }
+    ]
+  },
 
-    // Axios module configuration: https://go.nuxtjs.dev/config-axios
-    axios: {},
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
 
-    // PWA module configuration: https://go.nuxtjs.dev/pwa
-    pwa: {
-        manifest: {
-            lang: 'en'
-        }
-    },
+  srcDir: 'src',
 
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {}
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
 }
