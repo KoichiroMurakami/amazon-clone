79788ffbb417beef108731ee001840de3caade20
Rick516 <k.murakami516@gmail.com>
2021-01-25 18:48:35 +0900

    index modified

--- admin/src/pages/index.vue
+++ admin/src/pages/index.vue
@@ -8,37 +8,57 @@
             All Products
           </h1>
           <div class="a-spacing-large" />
-
-          <!-- button -->
+          <!-- Buttons -->
           <a href="#" class="a-button-buy-again">Add a new product</a>
-          <a href="#" class="a-button-history margin-right-10">Add a new category</a>
-          <a href="#" class="a-button-history margin-right-10">Add a new owner</a>
+          <a
+            href="#"
+            class="a-button-history margin-right-10"
+          >Add a new category</a>
+          <a
+            href="#"
+            class="a-button-history margin-right-10"
+          >Add a new owner</a>
         </div>
       </div>
     </div>
 
-    <!--  listing page -->
-    <div class="container-friud browsing-history">
+    <!--  Listing page -->
+    <div class="a-spacing-large" />
+    <div class="container-fluid browsing-history">
       <div class="row">
-        <div class="history-box">
-          <a href="#" class="a-link-normal">
-            <img src="#" class="img-fluid">
-          </a>
-
-          <div class="a-spacing-top-base asin-title">
-            <span class="a-text-normal">
-              <div class="p13n-sc-truncated">Product title</div>
-            </span>
-          </div>
-
-          <div class="a-row">
-            <a href="#">
-              <i class="fas fa-star" />
-              <i class="fas fa-star" />
-              <i class="fas fa-star" />
-              <i class="fas fa-star" />
-              <i class="fas fa-star" />
+        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+          <div class="history-box">
+            <a href="#" class="a-link-normal">
+              <img src="#" class="img-fluid">
             </a>
+            <div class="a-spacing-top-base asin-title">
+              <span class="a-text-normal">
+                <div class="p13n-sc-truncated">Product title</div>
+              </span>
+            </div>
+            <!-- Product rating -->
+            <div class="a-row">
+              <a href="#">
+                <i class="fas fa-star" />
+                <i class="fas fa-star" />
+                <i class="fas fa-star" />
+                <i class="fas fa-star" />
+                <i class="fas fa-star" />
+              </a>
+              <span class="a-letter-space" />
+              <span class="a-color-tertiary a-size-small asin-reviews">(2343)</span>
+            </div>
+            <!-- Product price -->
+            <div class="a-row">
+              <span class="a-size-base a-color-price">
+                <span class="p13n-sc-price">$34</span>
+              </span>
+            </div>
+            <!-- Product buttons -->
+            <div class="a-row">
+              <a href="#" class="a-button-history margin-right-10">Update</a>
+              <a href="#" class="a-button-history margin-right-10">Delete</a>
+            </div>
           </div>
         </div>
       </div>
