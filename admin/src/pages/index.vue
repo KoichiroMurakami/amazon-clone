9eb1948abf839f8c0686381f2d927fa6b3b6fe4a
Rick516 <k.murakami516@gmail.com>
2021-01-25 22:48:46 +0900

    admin product register page added

--- admin/src/pages/index.vue
+++ admin/src/pages/index.vue
@@ -26,14 +26,15 @@
     <div class="a-spacing-large" />
     <div class="container-fluid browsing-history">
       <div class="row">
-        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+        <div v-for="(product, index) in products" :key="product._id" class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
           <div class="history-box">
+            <!-- Product image -->
             <a href="#" class="a-link-normal">
-              <img src="#" class="img-fluid">
+              <img :src="product.photo" class="img-fluid img">
             </a>
             <div class="a-spacing-top-base asin-title">
               <span class="a-text-normal">
-                <div class="p13n-sc-truncated">Product title</div>
+                <div class="p13n-sc-truncated">{{ product.title }}</div>
               </span>
             </div>
             <!-- Product rating -->
@@ -51,7 +52,7 @@
             <!-- Product price -->
             <div class="a-row">
               <span class="a-size-base a-color-price">
-                <span class="p13n-sc-price">$34</span>
+                <span class="p13n-sc-price">${{ product.price }}</span>
               </span>
             </div>
             <!-- Product buttons -->
@@ -65,3 +66,26 @@
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  // thanks to ssr, asyncData is fetched before loading the page
+  async asyncData ({ $axios }) {
+    try {
+      const response = await $axios.$get('http://localhost:3000/api/products')
+      console.log(response)
+      return {
+        products: response.products
+      }
+    } catch (err) {
+
+    }
+  }
+}
+</script>
+<style">
+.img {
+  width: 500;
+  height: 700;
+}
+</style>
---
+++ admin/src/pages/product.vue
@@ -0,0 +1,84 @@
+<template>
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3" />
+        <div class="col-sm-6">
+          <div class="a-section">
+            <div class="a-spacing-top-medium" />
+            <h2 style="text-align: center">
+              Add A New Product
+            </h2>
+            <form>
+              <!-- Category dropdown -->
+              <div class="a-spacing-top-medium">
+                <label>Cateogry</label>
+                <select class="a-select-option">
+                  <option value="1">
+                    1
+                  </option>
+                  <option value="2">
+                    2
+                  </option>
+                </select>
+              </div>
+
+              <!--  Owner dropdown -->
+              <div class="a-spacing-top-medium">
+                <label>Owner</label>
+                <select class="a-select-option">
+                  <option value="1">
+                    1
+                  </option>
+                  <option value="2">
+                    2
+                  </option>
+                </select>
+              </div>
+              <!-- Title input -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px;">Title</label>
+                <input type="text" style="width: 100%" class="a-input-text">
+              </div>
+              <!-- Price input -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px">Price</label>
+                <input type="text" style="width: 100%" class="a-input-text">
+              </div>
+              <!-- Description textarea -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px">Price</label>
+                <textarea placeholder="Provide details such as a product description." style="width: 100%" />
+              </div>
+              <!-- Photo -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px">Add photo</label>
+                <div class="a-row a-spacing-top-medium">
+                  <label class="choosefile-button">
+                    <i class="fal fa-plus" />
+                    <input type="file">
+                    <p style="margin-top: -70px">photo</p>
+                  </label>
+                </div>
+              </div>
+              <!-- Register Button -->
+              <hr>
+              <div class="a-spacing-top-medium">
+                <span class="a-button-register">
+                  <span class="a-button-inner">
+                    <span class="a-button-text">Add Product</span>
+                  </span>
+                </span>
+              </div>
+            </form>
+          </div>
+          <div class="col-sm-3" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
