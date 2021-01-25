9d40a2b690520b79ecbc260942a98f1b35d91c00
Rick516 <k.murakami516@gmail.com>
2021-01-25 16:07:28 +0900

    product crud api added

--- server/routes/product.js
+++ server/routes/product.js
@@ -35,4 +35,86 @@ router.post('/products', upload.single('photo'), async (req, res) => {
   }
 });
 
+// get all products
+router.get('/products', async (req, res) => {
+  try {
+    let products = await Product.find();
+    res.json({
+      success: true,
+      products: products
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+// get a single product
+router.get('/products/:id', async (req, res) => {
+  try {
+    let product = await Product.findOne({ _id: req.params.id });
+    res.json({
+      success: true,
+      product: product
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+//  update a single product
+router.put('/products/:id', upload.single('photo'), async (req, res) => {
+  try {
+    let product = await Product.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          title: req.body.title,
+          price: req.body.price,
+          photo: req.file.location,
+          description: req.body.description,
+          stockQuantity: req.body.stockQuantity,
+          rating: req.body.rating,
+          category: req.body.categoryID,
+          owner: req.body.ownerID
+        }
+      },
+      { upsert: true }
+    );
+
+    res.json({
+      success: true,
+      updatedProduct: product
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+// delete a single product
+router.delete('/products/:id', async (req, res) => {
+  try {
+    let deletedProduct = await Product.findOneAndDelete({ _id: req.params.id });
+    if (deletedProduct) {
+      res.json({
+        success: true,
+        message: 'successfully deleted'
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
 module.exports = router;
