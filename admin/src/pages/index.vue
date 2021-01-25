33f9534125d3693329cccfb1eac4bd74717aa712
Rick516 <k.murakami516@gmail.com>
2021-01-25 17:53:45 +0900

    Revert "admin config added"
    This reverts commit f978b07104f5eb3a73a68b628d9c9c9f9401aa74.

--- admin/src/pages/index.vue
+++ admin/src/pages/index.vue
@@ -1,47 +1,51 @@
 <template>
-<main>
-  <div class="a-spacing-large"></div>
-  <div class="container-fluid browsing-history">
-    <div class="row">
-      <div class="col-sm-8 col-8">
-        <h1 class="a-size-large a-spacing-none a-text-normal">
-          All Products
-        </h1>
-        <div class="a-spacing-large"></div>
+  <main>
+    <div class="a-spacing-large"></div>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div class="col-sm-8 col-8">
+          <h1 class="a-size-large a-spacing-none a-text-normal">
+            All Products
+          </h1>
+          <div class="a-spacing-large"></div>
 
-        <!-- button -->
-        <a href="#" class="a-button-buy-again">Add a new product</a>
-        <a href="#" class="a-button-history margin-right-10">Add a new category</a>
-        <a href="#" class="a-button-history margin-right-10">Add a new owner</a>
+          <!-- button -->
+          <a href="#" class="a-button-buy-again">Add a new product</a>
+          <a href="#" class="a-button-history margin-right-10"
+            >Add a new category</a
+          >
+          <a href="#" class="a-button-history margin-right-10"
+            >Add a new owner</a
+          >
+        </div>
       </div>
     </div>
-  </div>
 
-  <!--  listing page -->
-  <div class="container-friud browsing-history">
-    <div class="row">
-      <div class="history-box">
-        <a href="#" class="a-link-normal">
-          <img src="#" class="img-fluid" />
-        </a>
-
-        <div class="a-spacing-top-base asin-title">
-          <span class="a-text-normal">
-            <div class="p13n-sc-truncated">Product title</div>
-          </span>
-        </div>
-
-        <div class="a-row">
-          <a href="#">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
+    <!--  listing page -->
+    <div class="container-friud browsing-history">
+      <div class="row">
+        <div class="history-box">
+          <a href="#" class="a-link-normal">
+            <img src="#" class="img-fluid" />
           </a>
-        </div>
+
+          <div class="a-spacing-top-base asin-title">
+            <span class="a-text-normal">
+              <div class="p13n-sc-truncated">Product title</div>
+            </span>
+          </div>
+
+          <div class="a-row">
+            <a href="#">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </a>
+          </div>
+          </div>
       </div>
     </div>
-  </div>
-</main>
+  </main>
 </template>
