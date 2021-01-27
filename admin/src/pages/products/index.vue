b33deb09f9044a0ba001275e77917c7c0e5ab645
Rick516 <k.murakami516@gmail.com>
2021-01-27 10:53:45 +0900

    client app added

---
+++ admin/src/pages/products/index.vue
@@ -0,0 +1,156 @@
+<template>
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3" />
+        <div class="col-sm-6">
+          <div class="a-section">
+            <div class="a-spacing-top-medium" />
+            <h2 style="text-align: center">
+              Add A New Product
+            </h2>
+            <form>
+              <!-- Category dropdown -->
+              <div class="a-spacing-top-medium">
+                <label>Category</label>
+                <select v-model="categoryID" class="a-select-option">
+                  <option
+                    v-for="category in categories"
+                    :key="category._id"
+                    :value="category._id"
+                  >
+                    {{ category.type }}
+                  </option>
+                </select>
+              </div>
+              <!--  Owner dropdown -->
+              <div class="a-spacing-top-medium">
+                <label>Owner</label>
+                <select v-model="ownerID" class="a-select-option">
+                  <option
+                    v-for="owner in owners"
+                    :key="owner._id"
+                    :value="owner._id"
+                  >
+                    {{ owner.name }}
+                  </option>
+                </select>
+              </div>
+              <!-- Title input -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px;">Title</label>
+                <input v-model="title" type="text" style="width: 100%" class="a-input-text">
+              </div>
+              <!-- Price input -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px">Price</label>
+                <input v-model="price" type="text" style="width: 100%" class="a-input-text">
+              </div>
+              <!-- Stock quantity input -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px">Stock quantity</label>
+                <input v-model="stockQuantity" type="text" style="width: 100%" class="a-input-text">
+              </div>
+              <!-- Description textarea -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px">Price</label>
+                <textarea
+                  v-model="description"
+                  placeholder="Provide details such as a product description."
+                  style="width: 100%"
+                />
+              </div>
+              <!-- Photo -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px; width: 100%;">Add photo</label>
+                <div class="a-row a-spacing-top-medium">
+                  <label class="choosefile-button" style="width: 100%">
+                    <i class="fal fa-plus" />
+                    <input type="file" @change="onFileSelected">
+                    <p style="margin-top: -70px; width: 100%; text-align: left">{{ fileName }}</p>
+                  </label>
+                </div>
+              </div>
+              <!-- Buttons -->
+              <hr>
+              <div class="a-spacing-top-medium">
+                <!-- register button -->
+                <span class="a-button-register">
+                  <span class="a-button-inner">
+                    <span class="a-button-text" @click="onAddProduct">Add Product</span>
+                  </span>
+                </span>
+                <!-- back to the top button -->
+                <span class="a-button-register">
+                  <span class="a-button-inner">
+                    <span class="a-button-text" @click="$router.back()">Back</span>
+                  </span>
+                </span>
+              </div>
+            </form>
+          </div>
+          <div class="col-sm-3" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios }) {
+    try {
+      const categories = $axios.$get('http://localhost:3000/api/categories')
+      const owners = $axios.$get('http://localhost:3000/api/owners')
+
+      const [catResponse, ownerResponse] = await Promise.all([
+        categories,
+        owners
+      ])
+      return {
+        categories: catResponse.categories,
+        owners: ownerResponse.owners
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  data () {
+    return {
+      categoryID: null,
+      ownerID: null,
+      title: '',
+      price: 0,
+      stockQuantity: 0,
+      rating: 0,
+      description: '',
+      selectedFile: null,
+      fileName: ''
+    }
+  },
+
+  methods: {
+    onFileSelected (evt) {
+      this.selectedFile = evt.target.files[0]
+      this.fileName = evt.target.files[0].name
+    },
+
+    async onAddProduct () {
+      // define format as form-data
+      const data = new FormData()
+      data.append('title', this.title)
+      data.append('price', this.price)
+      data.append('description', this.description)
+      data.append('ownerID', this.ownerID)
+      data.append('categoryID', this.categoryID)
+      data.append('photo', this.selectedFile, this.selectedFile.name)
+
+      await this.$axios.$post('http://localhost:3000/api/products', data)
+      // redirct user to the top page
+      this.$router.push('/')
+    }
+
+  }
+}
+</script>
---
+++ client/components/Navbar.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <header class="nav-opt-sprite nav-locate-us nav-lang-en nav-ssl nav-unrec">
+      <div class="container-fluid dekstop-nav">
+        <div class="row">
+          <!-- Logo -->
+          <div class="col-sm-2">
+            <div class="logo-area">
+              <a href="#">
+                <img src="/img/logo.png" alt="logo" class="img-fluid">
+              </a>
+            </div>
+          </div>
+          <!-- Search bar -->
+          <div class="col-sm-6 pt-0" />
+          <div class="col-sm-4" />
+        </div>
+        <div class="row">
+          <!-- delivery -->
+          <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 pl-2">
+            <div class="nav-global-location">
+              <nuxt-link to="/address" class="nav-a nav-a-2">
+                <div id="nav-packard-glow-loc-icon" class="nav-sprite" />
+                <div id="glow-ingress-block">
+                  <span id="glow-ingress-line1" class="nav-line-1">Deliver to</span>
+                  <span id="glow-ingress-line2" class="nav-line-2">California</span>
+                </div>
+              </nuxt-link>
+            </div>
+          </div>
+          <!-- Shopping -->
+          <div class="col-xl-6 col-lg-5 col-md-4 col-sm-6 p-0">
+            <div class="nav-fill">
+              <div class="nav-shop">
+                <nuxt-link to="/history" class="nav-a nav-a-2 nav-single-row-link">
+                  <span class="nav-line-2">Browsing History
+                    <span class="nav-icon nav-arrow" style="visibility:visible" />
+                  </span>
+                </nuxt-link>
+              </div>
+              <div class="nav-xshop-container">
+                <div class="nav-xshop">
+                  <a href="#" class="nav-a">Today's Deals</a>
+                  <a href="#" class="nav-a">Your Amazon.com</a>
+                  <a href="#" class="nav-a">Gift Cards</a>
+                  <a href="#" class="nav-a">Registry</a>
+                  <a href="#" class="nav-a">Sell</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Accounts and Order -->
+          <div class="col-xl-4 col-lg-5 col-md-6 col-sm-4 p-0">
+            <div class="nav-tools">
+              <a id="icp-nav-flyout" href="#" class="nav-a nav-a-2 icp-link-style-2">
+                <span class="icp-nav-link-inner">
+                  <span class="nav-line-1">
+                    <span class="icp-nav-globe-img-2" />
+                    <span class="icp-nav-language">EN</span>
+                  </span>
+                  <span class="nav-line-2">
+                    &nbsp;
+                    <span class="nav-icon nav-arrow" style="visibility:visible" />
+                  </span>
+                </span>
+              </a>
+              <span class="icp-nav-link-border" />
+              <nuxt-link
+                id="nav-link-accountList"
+                to="/register"
+                tabindex="0"
+                class="nav-a nav-a-2"
+              >
+                <span class="nav-line-1">Hello, Sign in</span>
+                <span class="nav-line-2">
+                  Account &amp; Lists
+                  <span class="nav-icon nav-arrow" style="visibility: visible" />
+                </span>
+              </nuxt-link>
+              <nuxt-link to="/orders" class="nav-a nav-a-2 nav-single-row-link">
+                <span class="nav-line-1" />
+                <span class="nav-line-2">Orders</span>
+              </nuxt-link>
+              <nuxt-link id="nav-cart" to="/cart" class="nav-a nav-a-2">
+                <span class="nav-line-1" aria-hidden="true" />
+                <span class="nav-line-2" aria-hidden="true">Cart</span>
+                <span class="nav-sprite nav-cart-icon" />
+                <span id="nav-cart-count" class="nav-cart-count nav-cart-0" aria-hidden="true">0</span>
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
