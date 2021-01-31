b21334fece93ba4ea0d6b0bc61de66322d7dacfe
Rick516 <k.murakami516@gmail.com>
2021-01-31 19:11:11 +0900

    review page added

--- server/routes/review.js
+++ server/routes/review.js
@@ -1,23 +1,21 @@
-const router = require('express').Router()
-const Review = require('../models/review')
-const Product = require('../models/product')
-const uploadPhoto = require('../middlewares/uploadPhoto')
-const verifyToken = require('../middlewares/verifyToken')
+const router = require('express').Router();
+const Review = require('../models/review');
+const Product = require('../models/product');
+const uploadPhoto = require('../middlewares/uploadPhoto');
+const verifyToken = require('../middlewares/verifyToken');
 
-router.post('/reviews/:productID',
-  [verifyToken, uploadPhoto.single('photo')],
-  async (req, res) => {
+router.post('/reviews/:productID', [verifyToken, uploadPhoto.single('photo')], async (req, res) => {
   try {
-    const review = new Review()
-    review.headline = req.body.headline
-    review.body = req.body.body
-    review.rating = req.body.rating
-    review.photo = req.file.location
-    review.user = req.decoded._id
-    review.productID = req.params.productID
+    const review = new Review();
+    review.headline = req.body.headline;
+    review.body = req.body.body;
+    review.rating = req.body.rating;
+    review.photo = req.file.location;
+    review.user = req.decoded._id;
+    review.productID = req.params.productID;
 
-    await Product.updateOne({ $push: review._id})
-    const savedReview = await review.save()
+    await Product.updateOne({ $push: review._id});
+    const savedReview = await review.save();
 
     if (savedReview) {
       res.json({
@@ -32,3 +30,24 @@ router.post('/reviews/:productID',
     });
   }
 })
+
+router.get('/reviews/:productID', async (req, res) => {
+  try {
+    const productReviews = await Review.findOne({
+      productID: req.params.productID
+    }).populate('user').exec();
+
+    res.json({
+      success: true,
+      reviews: productReviews
+    })
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
+})
+
+module.exports = router;
